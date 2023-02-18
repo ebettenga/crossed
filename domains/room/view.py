00c7eb485b5c831ab576347e556154d503095680
Ethan Bettenga <ethan@listenersoncall.com>
2023-02-17 23:27:03 -0600

    making socket stuff work a bit

--- domains/room/view.py
+++ domains/room/view.py
@@ -1,5 +1,5 @@
 from flask import request, session
-from flask_socketio import Namespace
+
 from marshmallow import ValidationError
 from config import socketio
 from domains.authentication.service import login_required
@@ -11,7 +11,7 @@ from domains.user.model import *
 
 
 from config import socketio
-from flask_socketio import emit, join_room, send
+from flask_socketio import emit, join_room
 
 
 room_service = RoomService()
@@ -45,56 +45,31 @@ def join_room(join_id):
     return redirect(f"http://localhost:5173/join/{room.id}")
 
 
-class RoomNamespace(Namespace):
-    def on_connect(self):
-        print(session)
-        """event listener when client connects to the server"""
-        print(request.sid)
-        print("client has connected")
-        emit("connect", {"data": f"id: {request.sid} is connected"})
-
-    def on_disconnect(self):
-        """event listener when client disconnects to the server"""
-        print("user disconnected")
-        emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
-
-    def on_error(self, e):
-        print(request.event["message"])  # "my error event"
-        print(request.event["args"])
-        print(e)
-
-    """
-        Steps for playing a game
-        1. user 1 picks a crossword
-        2. user 1 gets a join id, which they can pass to other users
-        3. user 1 joins a roomsocket
-        4. user 2 hits endpoint sent to them to join the room
-        5. user 2 gets redirected to the website, which grabs the room id from the path
-        6. user 2 joins the roomsocket
-        7. since two players have now joined the room, a message is emitted to the room telling the game to start
-        8. the game starts
+@socketio.on("connect")
+def connected():
+    """event listener when client connects to the server"""
+    print(request.sid)
+    print("client has connected")
+    emit("connect", {"data": f"id: {request.sid} is connected"})
 
 
-        Steps while playing
-        1. user 1 submits room_number, col, row, and letter guessed throught the socket
-        2. server checks that the letter hasn't been guessed before, and determines whether it's correct
-        3. server updates the players scores and emits the new scores, if the guess was correct, it also emits the found letters
-    """
-
-    def on_join(data):
-        username = data["username"]
-        room = data["room"]
-        join_room(room)
-        print(room)
-        send(username + " has entered the room.", to=room)
-
-    def on_game(self, data):
-        """event listener when client types a message"""
-        room = data["room"]
-        join_room(room)
-        print(room)
-        print("data from the front end: ", str(data))
-        send({"data": data, "id": request.sid}, to=room)
+@socketio.on("join")
+def on_join(data):
+    room = data["room"]
+    join_room(room)
+    emit("room_joined", "player has joined")
 
 
-socketio.on_namespace(RoomNamespace("/"))
+@socketio.on("message")
+def handle_message(data):
+    print("got message", data)
+    room = data["room"]
+    """event listener when client types a message"""
+    emit("message", {"message": "Hello World"})
+
+
+@socketio.on("disconnect")
+def disconnected():
+    """event listener when client disconnects to the server"""
+    print("user disconnected")
+    emit("disconnect", f"user {request.sid} disconnected", broadcast=True)
